0384f35fcd9049317060fc4dc0fcdb085707fe21
Matthew Harding <matthewpharding@gmail.com>
2022-10-18 14:56:58 +0100

    Corrected all compilation issues with the Xcode playground and transfered the lesson description into the README file

--- Error Handling.playground/Pages/Error Handling.xcplaygroundpage/Contents.swift
+++ Error Handling.playground/Pages/Error Handling.xcplaygroundpage/Contents.swift
@@ -17,13 +17,15 @@
 //: In Swift, errors are represented by values of types that conform to the Error protocol. This empty protocol indicates that a type can be used for error handling.
 //:
 //: Swift enumerations are particularly well suited to modeling a group of related error conditions, with associated values allowing for additional information about the nature of an error to be communicated. For example, here’s how you might represent the error conditions of operating a vending machine inside a game:
+import Foundation
 enum VendingMachineError: Error {
     case invalidSelection
     case insufficientFunds(coinsNeeded: Int)
     case outOfStock
 }
 //: Throwing an error lets you indicate that something unexpected happened and the normal flow of execution can’t continue. You use a throw statement to throw an error. For example, the following code throws an error to indicate that five additional coins are needed by the vending machine:
-throw VendingMachineError.insufficientFunds(coinsNeeded: 5)
+// ⛔️ Run time error: uncomment to view
+//throw VendingMachineError.insufficientFunds(coinsNeeded: 5)
 //: ## Handling Errors
 //:
 //: When an error is thrown, some surrounding piece of code must be responsible for handling the error—for example, by correcting the problem, trying an alternative approach, or informing the user of the failure.
@@ -38,9 +40,9 @@ throw VendingMachineError.insufficientFunds(coinsNeeded: 5)
 //: ### Propagating Errors Using Throwing Functions
 //:
 //: To indicate that a function, method, or initializer can throw an error, you write the throws keyword in the function’s declaration after its parameters. A function marked with throws is called a throwing function. If the function specifies a return type, you write the throws keyword before the return arrow (->).
-func canThrowErrors() throws -> String
+func canThrowErrors() throws -> String { return "" }
 
-func cannotThrowErrors() -> String
+func cannotThrowErrors() -> String { return "" }
 //: A throwing function propagates errors that are thrown inside of it to the scope from which it’s called.
 //:
 //: * callout(Note):
@@ -109,7 +111,7 @@ struct PurchasedSnack {
 //: You use a do-catch statement to handle errors by running a block of code. If an error is thrown by the code in the do clause, it’s matched against the catch clauses to determine which one of them can handle the error.
 //:
 //: Here is the general form of a do-catch statement:
-do {
+/* do {
     try expression
     statements
 } catch pattern 1 {
@@ -121,6 +123,7 @@ do {
 } catch {
     statements
 }
+ */
 //: You write a pattern after catch to indicate what errors that clause can handle. If a catch clause doesn’t have a pattern, the clause matches any error and binds the error to a local constant named error. For more information about pattern matching, see Patterns.
 //:
 //: For example, the following code matches against all three cases of the VendingMachineError enumeration.
@@ -174,7 +177,8 @@ func eat(item: String) throws {
 //:
 //: You use try? to handle an error by converting it to an optional value. If an error is thrown while evaluating the try? expression, the value of the expression is nil. For example, in the following code x and y have the same value and behavior:
 func someThrowingFunction() throws -> Int {
-    // ...
+    // do something
+    return 0
 }
 
 let x = try? someThrowingFunction()
@@ -188,6 +192,10 @@ do {
 //: If someThrowingFunction() throws an error, the value of x and y is nil. Otherwise, the value of x and y is the value that the function returned. Note that x and y are an optional of whatever type someThrowingFunction() returns. Here the function returns an integer, so x and y are optional integers.
 //: 
 //: Using try? lets you write concise error handling code when you want to handle all errors in the same way. For example, the following code uses several approaches to fetch data, or returns nil if all of the approaches fail.
+
+func fetchDataFromDisk() throws -> Data { return Data() }
+func fetchDataFromServer() throws -> Data { return Data() }
+
 func fetchData() -> Data? {
     if let data = try? fetchDataFromDisk() { return data }
     if let data = try? fetchDataFromServer() { return data }
@@ -198,12 +206,44 @@ func fetchData() -> Data? {
 //: Sometimes you know a throwing function or method won’t, in fact, throw an error at runtime. On those occasions, you can write try! before the expression to disable error propagation and wrap the call in a runtime assertion that no error will be thrown. If an error actually is thrown, you’ll get a runtime error.
 //:
 //: For example, the following code uses a loadImage(atPath:) function, which loads the image resource at a given path or throws an error if the image can’t be loaded. In this case, because the image is shipped with the application, no error will be thrown at runtime, so it’s appropriate to disable error propagation.
+func loadImage(atPath path: String) throws -> Data { return Data() }
 let photo = try! loadImage(atPath: "./Resources/John Appleseed.jpg")
 //: ## Specifying Cleanup Actions
 //:
 //: You use a defer statement to execute a set of statements just before code execution leaves the current block of code. This statement lets you do any necessary cleanup that should be performed regardless of how execution leaves the current block of code—whether it leaves because an error was thrown or because of a statement such as return or break. For example, you can use a defer statement to ensure that file descriptors are closed and manually allocated memory is freed.
 //:
 //: A defer statement defers execution until the current scope is exited. This statement consists of the defer keyword and the statements to be executed later. The deferred statements may not contain any code that would transfer control out of the statements, such as a break or a return statement, or by throwing an error. Deferred actions are executed in the reverse of the order that they’re written in your source code. That is, the code in the first defer statement executes last, the code in the second defer statement executes second to last, and so on. The last defer statement in source code order executes first.
+
+class File {
+    enum ReadError: Error {
+        case indexOutOfBounds
+    }
+    var index = 0
+    let text = """
+    start
+    line 1
+    line 2
+    line 3
+    end
+    """
+    func readline() throws -> String? {
+        let lines = text.components(separatedBy: CharacterSet.newlines)
+        if lines.indices.contains(index) == false {
+            throw ReadError.indexOutOfBounds
+        }
+        defer { index += 1 }
+        if lines.indices.contains(index) {
+            return lines[index]
+        }
+        return nil
+    }
+    
+    func reset() { index = 0 }
+}
+func exists(_ filename: String) -> Bool { return true }
+func open(_ filename: String) -> File { return File() }
+func close(_ file: File) { file.reset() }
+
 func processFile(filename: String) throws {
     if exists(filename) {
         let file = open(filename)
@@ -212,11 +252,20 @@ func processFile(filename: String) throws {
         }
         while let line = try file.readline() {
             // Work with the file.
+            print(line)
         }
-        // close(file) is called here, at the end of the scope.
+        // 👉 close(file) is called here, at the end of the scope.
     }
+}
+
+do {
+    try processFile(filename: "test")
+} catch {
+    
 }
 //: The above example uses a defer statement to ensure that the open(_:) function has a corresponding call to close(_:).
 //:
 //: * callout(Note):
 //:     → You can use a defer statement even when no error handling code is involved.
+
+
